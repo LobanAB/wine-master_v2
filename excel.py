3f118b43eb3ee59e5909acd0301eabb1b9fd5431
LobanAB <lobanab@iassets.ru>
2020-07-11 18:06:26 +0300

    Done

--- excel.py
+++ excel.py
@@ -1,6 +1,19 @@
 import pandas
 import xlrd
+from pprint import pprint
+import collections
 
-excel_data = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='record')
+excel_data = pandas.read_excel('wine3.xlsx', sheet_name='Лист1').to_dict(orient='index')
 
-print(excel_data)
+output = collections.defaultdict(dict)
+#category = []
+#for key, value in excel_data.items():
+#    value['Категория']
+#    category.append(value['Категория'])
+#category = list(set(category))
+for key, value in excel_data.items():
+    output[value['Категория']][len(output[value['Категория']])] = value
+
+pprint(output)
+
+print(output.keys())
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import xlrd
+import collections
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -16,13 +17,15 @@ today = datetime.datetime(year=now_year, month=now_month, day=now_day)
 estimate = datetime.datetime(year=1920, month=1, day=1)
 delta_years = today.year - estimate.year
 
-excel_data = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='record')
-
+excel_data = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', keep_default_na=False).to_dict(orient='index')
+output = collections.defaultdict(dict)
+for key, value in excel_data.items():
+    output[value['Категория']][len(output[value['Категория']])] = value
 
 template = env.get_template('template.html')
 rendered_page = template.render(
     date_delta=delta_years,
-    wines=excel_data,
+    wines=output,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:

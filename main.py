67ddad909de3afce33cc044028c1fbd8e3465c21
LobanAB <lobanab@iassets.ru>
2020-07-11 18:11:03 +0300

    Done

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
-import xlrd
 import collections
 
 env = Environment(
